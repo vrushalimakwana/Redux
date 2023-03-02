72cc89ed0621f6464e184f1f54c596514cb9fba5
vrushalimakwana <vrushali.makwana@ghanshyamdigital.com>
2023-03-02 19:01:29 +0530

    Components

---
+++ src/Components/Products.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux';
+import {add} from '../Store/cartSlice';
+
+const Products = () => {
+
+const dispatch = useDispatch();
+
+    const [products,setProducts] = useState([]);
+
+    useEffect(()=> {
+        const fetchProducts = async ()=> {
+            const res = await fetch('https://fakestoreapi.com/products');
+            const data = await res.json();
+            console.log(data);
+            setProducts(data);
+
+        }
+        fetchProducts();
+    }, []);
+const handleAdd = (product) => {
+    dispatch(add(product));
+
+
+}
+
+  return (
+    <div className='products'>
+        {
+            products.map(product => (
+                <div className='card' key={product.id}>
+                    <img src={product.image} alt='' />
+                    <h4>{product.title}</h4>
+                    <h5>{product.price}</h5>
+                    <button  onClick={() => handleAdd(product)} className='btn'>Add to cart</button>
+                </div>
+            ))
+        }
+      
+    </div>
+  )
+}
+
+export default Products;
